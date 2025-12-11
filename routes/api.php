9875f7140098da5900d3bf884b894c83fb0568a7
ramashankar <ramashankar.yadav@markupmail.com>
2025-12-10 23:11:53 -0800

    Changes in routes

--- routes/api.php
+++ routes/api.php
@@ -10,9 +10,9 @@ Route::post('/auth/request-otp', [AuthController::class, 'login']);
 Route::post('/auth/verify-otp', [AuthController::class, 'verifyOtp']);
 Route::middleware('auth:sanctum')->group(function () {
 
-    // Profile APIs
-    Route::get('/profile', [ProfileController::class, 'getProfile']);
-    Route::put('/profile', [ProfileController::class, 'updateProfile']);
+// Profile APIs
+Route::get('/profile', [ProfileController::class, 'getProfile']);
+Route::put('/profile', [ProfileController::class, 'updateProfile']);
+Route::post('/auth/logout', [AuthController::class, 'logout']);
 
-    Route::post('/auth/logout', [AuthController::class, 'logout']);
 });
